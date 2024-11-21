bc49b1e8078dc7393d00b44c8e550a7434803334
Jared De Blander <jared0x90@gmail.com>
2024-11-21 05:15:42 -0500

    modules and concat

---
+++ 20-Module.sv
@@ -0,0 +1,14 @@
+module top_module ( input a, input b, output out );
+
+    // instantiate my_module by name.
+    // notice the period immediately preceding the
+    // port name in this syntax. this will NOT break
+    // if the order of the ports in my_module changes
+    // mod_a my_module (.in1(a), .in2(b), .out(out));
+
+    // instantiate my_module by order.
+    // this will break if the order of the ports
+    // in my_module ever changes
+    mod_a my_module (a, b, out);
+
+endmodule
