8a01fafc0f767cc9f5910681ab0ef6a1edd73b83
Jared De Blander <jared0x90@gmail.com>
2024-11-25 10:48:21 -0500

    my updated solution that requires no if.

--- 41-Popcount255.sv
+++ 41-Popcount255.sv
@@ -7,11 +7,9 @@ module top_module(
     output [7:0] out );
 
     always @(*) begin
-        out = 8'b0;
+        out = 0;
         for (int i = 0; i < 255; i++) begin
-            if (in[i]) begin
-                out = out + 1;
-            end
+            out = out + in[i];
         end
     end
 
