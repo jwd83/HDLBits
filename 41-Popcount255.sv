380154e3e7616b28edfc1bfc98730779134182f0
Jared De Blander <jared0x90@gmail.com>
2024-11-25 10:40:03 -0500

    starting popcount

---
+++ 41-Popcount255.sv
@@ -0,0 +1,9 @@
+// A "population count" circuit counts the number of '1's in an input vector.
+// Build a population count circuit for a 255-bit input vector.
+
+
+module top_module(
+    input [254:0] in,
+    output [7:0] out );
+
+endmodule
