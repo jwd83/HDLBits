0488aecc3e34be6f92e2400bf135b9e325d114b3
Jared De Blander <jared0x90@gmail.com>
2024-11-27 11:37:03 -0500

    my product of sums worked first try again!

---
+++ 75-kmap3.sv
@@ -0,0 +1,56 @@
+/*
+
+a b c d | out
+-------------
+0 0 0 0 | 0
+0 0 0 1 | 0
+0 0 1 0 | 1
+0 0 1 1 | 1
+
+0 1 0 0 | ?
+0 1 0 1 | 0
+0 1 1 0 | 0
+0 1 1 1 | 0
+
+1 0 0 0 | 1
+1 0 0 1 | ?
+1 0 1 0 | 1
+1 0 1 1 | 1
+
+1 1 0 0 | 1
+1 1 0 1 | ?
+1 1 1 0 | 1
+1 1 1 1 | 1
+
+
+product of sums
+a b c d | out
+-------------
+0 0 0 0 | 0
+0 0 0 1 | 0
+0 1 0 1 | 0
+0 1 1 0 | 0
+0 1 1 1 | 0
+
+out = ( a |  b |  c |  d) &
+      ( a |  b |  c | ~d) &
+      ( a | ~b |  c | ~d) &
+      ( a | ~b | ~c |  d) &
+      ( a | ~b | ~c | ~d);
+
+*/
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out  );
+
+    assign out =
+        ( a |  b |  c |  d) &
+        ( a |  b |  c | ~d) &
+        ( a | ~b |  c | ~d) &
+        ( a | ~b | ~c |  d) &
+        ( a | ~b | ~c | ~d);
+
+endmodule
