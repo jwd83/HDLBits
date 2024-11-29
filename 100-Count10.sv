770af1af2f31d3d0492019d6b4f4720ea46d1e33
Jared De Blander <jared0x90@gmail.com>
2024-11-29 18:37:59 -0500

    decade counter

---
+++ 100-Count10.sv
@@ -0,0 +1,22 @@
+// Build a decade counter that counts from 0 through 9, inclusive, with a period
+// of 10. The reset input is synchronous, and should reset the counter to 0.
+
+
+module top_module (
+    input clk,
+    input reset,        // Synchronous active-high reset
+    output [3:0] q);
+
+
+    always_ff @(posedge clk ) begin
+        if (reset)
+            q <= 0;
+        else
+            if (q == 9)
+                q <= 0;
+            else
+                q <= q + 1;
+
+    end
+
+endmodule
