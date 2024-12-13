57a6f13f93dc838f0e647aea85d5d16d67f35473
Jared De Blander <jared@ayrscott.com>
2024-12-13 17:30:34 -0500

    starting notes on lfsr32

--- 107-Circuits-SequentialLogic-ShiftRegisters/112-Lfsr32.sv
+++ 107-Circuits-SequentialLogic-ShiftRegisters/112-Lfsr32.sv
@@ -1,3 +1,17 @@
+/*
+
+Taken from 2015 midterm question 5. See also the first part of this question:
+mt2015_muxdff
+
+Write the Verilog code for this sequential circuit (Submodules are ok, but the
+top-level must be named top_module). Assume that you are going to implement the
+circuit on the DE1-SoC board. Connect the R inputs to the SW switches, connect
+Clock to KEY[0], and L to KEY[1]. Connect the Q outputs to the red lights LEDR.
+
+*/
+
+
+
 module top_module(
     input clk,
     input reset,    // Active-high synchronous reset to 32'h1
