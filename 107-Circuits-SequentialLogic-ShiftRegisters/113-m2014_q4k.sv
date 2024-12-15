173f97d52e082cbd2a94f3dc844645a31ebe65e5
Jared De Blander <jared0x90@gmail.com>
2024-12-15 00:28:07 -0500

    more fun with LFSR

--- 107-Circuits-SequentialLogic-ShiftRegisters/113-m2014_q4k.sv
+++ 107-Circuits-SequentialLogic-ShiftRegisters/113-m2014_q4k.sv
@@ -10,4 +10,21 @@ module top_module (
     input in,
     output out);
 
+    logic [3:0] q;
+
+    always_comb begin
+        out = q[3];
+    end
+
+    always_ff @(posedge clk) begin
+        if (!resetn) begin
+            q <= 4'b0;
+        end else begin
+            q[3] <= q[2];
+            q[2] <= q[1];
+            q[1] <= q[0];
+            q[0] <= in;
+        end
+    end
+
 endmodule
