5dc76f9a6a9e0dca546c6d363bbdb15c5d846ba1
Jared De Blander <jared0x90@gmail.com>
2024-12-14 23:43:19 -0500

    adding notes

--- 107-Circuits-SequentialLogic-ShiftRegisters/111-Mt2015_lfsr.sv
+++ 107-Circuits-SequentialLogic-ShiftRegisters/111-Mt2015_lfsr.sv
@@ -9,7 +9,30 @@ circuit on the DE1-SoC board. Connect the R inputs to the SW switches, connect
 Clock to KEY[0], and L to KEY[1]. Connect the Q outputs to the red lights LEDR.
 
 The first part of this question is here in this repo:
-091-Mt2015_muxdff.sv
+------------------------------------------------------------------------------
+	091-Mt2015_muxdff.sv :
+------------------------------------------------------------------------------
+
+module top_module (
+	input clk,
+	input L,
+	input r_in,
+	input q_in,
+	output reg Q);
+
+	always_ff @(posedge clk) begin
+        if (L) begin
+            // r_in is latched when L is 1
+            Q = r_in; // 0
+        end else begin
+            // q_in is latched when L is 0
+
+            Q = q_in;
+        end
+    end
+
+endmodule
+
 
 */
 
